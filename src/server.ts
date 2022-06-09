ae0f9c709456ec3c1ab31a21cc509938281b4638
mizuirorivi <onodaha@gmail.com>
2022-06-09 22:42:19 +0900

    graphqlのvalidationルールの追加

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,23 @@
 import * as express from "express";
 import * as expressGraphQL from 'express-graphql';
 import { env } from './environment/environment';
-import { books,authors} from "./data/data";
-import {RootQueryType } from './type/type'
-import {schema} from "./schema/schema";
+import { schema } from "./schema/schema";
+import { createComplexityLimitRule } from 'graphql-validation-complexity';
+import { GraphQLError } from "graphql";
 
-const {
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLList,
-  GraphQLInt,
-  GraphQLNonNull
-} = require('graphql')
+// create express application
 const app = express.default();
+
+// create validation rule for graphql
+const ComplexityimitRule = createComplexityLimitRule(1000, {
+  scalarCost: 10,
+  objectCost: 10, // Default is 0.
+});
 
 app.use('/graphql', expressGraphQL.graphqlHTTP({
   schema: schema,
+  // limit query nest
+  validationRules: [ComplexityimitRule],
   graphiql: true
 }))
 app.listen(env.host.port ? env.host.port : 8888, () => console.log('Server Running'))
